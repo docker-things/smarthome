9e17e812799f2dee086a8d8677a8973a3386bb3d
Gabriel Ionescu <gabi.ionescu+github@protonmail.com>
2019-08-26 01:16:17 +0300

    Make sure bluetooth is usable when the service starts

--- app/modules/bluetooth-scan/bluetooth-scan.sh
+++ app/modules/bluetooth-scan/bluetooth-scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while [ 1 == 1 ]; do
-	timeout -t 2 -s SIGINT hcitool lescan | ash hcitool-lescan-to-mqtt.sh
+	timeout -t 2 -s SIGINT hcitool lescan 2>&1 | ash hcitool-lescan-to-mqtt.sh
 	sleep 30
 done
