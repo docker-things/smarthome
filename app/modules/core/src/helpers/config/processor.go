a190c3b20d343c80fb73c97eb6f89af7b2ba38ab
Gabi Ionescu <gabi.ionescu+git@protonmail.com>
2020-08-09 10:02:38 +0300

    Switch to proper logging

--- app/modules/core/src/helpers/config/processor.go
+++ app/modules/core/src/helpers/config/processor.go
@@ -15,6 +15,7 @@ import (
 
   deepcopy "../deepcopy"
   json "../json"
+  log "../log"
 )
 
 var regex map[string]*regexp.Regexp
@@ -23,17 +24,15 @@ func dumpPath(path string, tree map[string]interface{}) {
   where := strings.Split(path, ".")
   value, err := getAbsoluteTreeValue(where, tree)
   if err != nil {
-    fmt.Printf("\n[DUMP] %s: %s\n", path, err.Error())
+    log.Dump(path, err.Error())
   } else {
-    fmt.Printf("\n[DUMP][%p] %s:\n%s\n", value, path, json.Encode(value))
+    log.Dump(value, path)
   }
 }
 
-func getPointer(value interface{}) string {
-  return fmt.Sprintf("%p", value)
-}
-
 func processConfig(config map[string]interface{}) {
+  log.Init()
+
   preProcessVariablesIn(config, []string{}, config, []string{ /* EXCEPT FOR */
     "PARAMS",
     "RESPONSE",
@@ -41,11 +40,6 @@ func processConfig(config map[string]interface{}) {
     "Properties",
   })
   preProcessObjectsIn(config)
-  preProcessVariablesIn(config, []string{}, config, []string{ /* EXCEPT FOR */
-    "PARAMS",
-    "RESPONSE",
-    "ARGS",
-  })
 
   // TODO: Do the rest of the processing!!!
 
@@ -63,8 +57,7 @@ func preProcessObjectsIn(fullConfig map[string]interface{}) {
   // Get objects
   objectsInterface, wtfErr := getAbsoluteTreeValue([]string{"Objects"}, fullConfig)
   if wtfErr != nil {
-    fmt.Println("config:processor:preProcessObjectsIn(): " + wtfErr.Error())
-    panic("config:processor:preProcessObjectsIn(): WTF! This should... like... NEVER HAPPEN!!!")
+    log.Panic("WTF! This should... like... NEVER HAPPEN!!!\n" + wtfErr.Error())
   }
   objects := objectsInterface.(map[string]interface{})
 
@@ -74,14 +67,13 @@ func preProcessObjectsIn(fullConfig map[string]interface{}) {
 
     // Why no base?
     if _, ok := object["base"]; !ok {
-      panic("config.processor.preProcessObjectsIn(): Object " + objectName + " has no base!")
+      log.Panic("Object " + objectName + " has no base!")
     }
 
     // Get base config
     baseConfig, err := getAbsoluteTreeValue(strings.Split(object["base"].(string), "."), fullConfig)
     if err != nil {
-      fmt.Println("config.processor.preProcessObjectsIn(): Couldn't find the base config for object '" + objectName + "'")
-      panic("config:processor:preProcessObjectsIn(): " + err.Error())
+      log.Panic("Couldn't find the base config for object '" + objectName + "'\nError: " + err.Error())
     }
 
     // Set base config
@@ -107,10 +99,21 @@ func preProcessObjectsIn(fullConfig map[string]interface{}) {
 
     // Replace object definition with the newly built one
     objects[objectName] = base
+
+    preProcessVariablesIn(objects, []string{"Objects"}, fullConfig, []string{ /* EXCEPT FOR */
+      "PARAMS",
+      "RESPONSE",
+      "ARGS",
+    })
   }
 }
 
 func preProcessVariablesIn(config interface{}, path []string, fullConfig map[string]interface{}, except []string) {
+  log.PushPath("preProcessVariablesIn")
+  defer log.PopPath()
+
+  log.Info(strings.Join(path, "."))
+
   switch reflect.ValueOf(config).Kind().String() {
   case "map":
     for k, v := range config.(map[string]interface{}) {
@@ -132,28 +135,17 @@ func preProcessVariablesIn(config interface{}, path []string, fullConfig map[str
     }
   default: // TODO: REMOVE
     if reflect.ValueOf(config).Kind().String() != "float64" {
-      fmt.Printf("UNKNOWN TYPE: '%s' @ %s\n", reflect.ValueOf(config).Kind().String(), json.Encode(path))
+      log.Warn("UNKNOWN TYPE: '" + reflect.ValueOf(config).Kind().String() + "' @ " + json.Encode(path))
     }
   }
-}
-
-func preProcessRegexp() {
-  regex = make(map[string]*regexp.Regexp)
-  regex["variable"] = regexp.MustCompile("\\${([a-zA-Z0-9.-]+)}")
-}
-
-func contains(searchTerm string, list []string) bool {
-  for _, value := range list {
-    if value == searchTerm {
-      return true
-    }
-  }
-  return false
 }
 
 func preProcessString(str string, path []string, fullConfig map[string]interface{}, except []string) string {
+  log.PushPath("preProcessString")
+  defer log.PopPath()
+
   // strings.Join(path, ".") == "Module.Heating.Cron.jobs.0.run.0"
-  fmt.Println("preProcessString(): " + str + " in " + json.Encode(path))
+  log.Info("\"" + str + "\" in " + json.Encode(path))
   matches := regex["variable"].FindAllStringSubmatch(str, -1)
   if len(matches) == 0 || matches[0][1] == "" {
     return str
@@ -171,36 +163,38 @@ func preProcessString(str string, path []string, fullConfig map[string]interface
 
     value, err := getClosestTreeValue(strings.Split(match[1], "."), path, fullConfig)
     if err != nil {
-      panic("config:processor:preProcessString(): VARIABLE NOT FOUND!\nSEARCH = " + match[1] + "\nPATH = " + strings.Join(path, "."))
+      log.Panic("VARIABLE NOT FOUND!\nSEARCH = " + match[1] + "\nPATH = " + strings.Join(path, ".") + "\nERROR = " + err.Error())
     }
     if reflect.ValueOf(value).Kind().String() == "float64" {
       value = fmt.Sprintf("%g", value)
     } else if reflect.ValueOf(value).Kind().String() != "string" {
-      panic("config:processor:preProcessString(): Variable found is not a string! It's '" + reflect.ValueOf(value).String() + "'\nSEARCH = " + match[1] + "\nPATH = " + strings.Join(path, "."))
+      log.Panic("Variable found is not a string! It's '" + reflect.ValueOf(value).String() + "'\nSEARCH = " + match[1] + "\nPATH = " + strings.Join(path, "."))
     }
 
     newStr = strings.Replace(newStr, match[0], value.(string), -1)
-
-    // fmt.Printf("\nPATH: %s\n", strings.Join(path, "."))
-    // fmt.Printf("FROM: \"%s\"\nTO:   \"%s\"\n", str, newStr)
   }
 
   return newStr
 }
 
 func getClosestTreeValue(requiredPath []string, currentPath []string, fullConfig map[string]interface{}) (interface{}, error) {
-  // fmt.Printf("\nconfig:processor:getClosestTreeValue(): %s, %s\n", json.Encode(requiredPath), json.Encode(currentPath))
-  // fmt.Println("currentPath = " + json.Encode(currentPath))
+  log.PushPath("getClosestTreeValue")
+  defer log.PopPath()
+  log.Info("Search for \"" + strings.Join(requiredPath, ".") + "\"")
+
+  log.Deactivate()
+
   for len(currentPath) != 0 {
     // Go up a level
     currentPath = currentPath[:len(currentPath)-1]
-    // fmt.Println("currentPath = " + json.Encode(currentPath))
+    log.Info("In: " + strings.Join(currentPath, "."))
 
     // Get current path in which we should search
+    log.IgnoreFrom()
     currentPathConfig, wtfErr := getAbsoluteTreeValue(currentPath, fullConfig)
+    log.IgnoreTo()
     if wtfErr != nil {
-      fmt.Println("config:processor:getClosestTreeValue(): " + wtfErr.Error())
-      panic("config:processor:getClosestTreeValue(): WTF! This should... like... NEVER HAPPEN!!!")
+      log.Panic("WTF! This should... like... NEVER HAPPEN!!!\n" + wtfErr.Error())
     }
 
     // Search for the required path
@@ -208,41 +202,47 @@ func getClosestTreeValue(requiredPath []string, currentPath []string, fullConfig
 
     // If value found return it
     if err == nil {
-      // fmt.Println("FOUND " + json.Encode(currentPath) + "." + json.Encode(requiredPath) + " = " + json.Encode(requiredPathValue))
+      log.Info("FOUND " + json.Encode(currentPath) + "." + json.Encode(requiredPath) + " = " + json.Encode(requiredPathValue))
+      log.Activate()
       return requiredPathValue, nil
     }
-    // fmt.Println("Not found: " + strings.Join(requiredPath, "."))
+    // log.Warn("Not found")
   }
 
-  // fmt.Println("config:processor:getClosestTreeValue(): Path not found: " + strings.Join(requiredPath, "."))
+  log.ShowDeactivatedMessages()
+  log.Activate()
+
   // Shit happens
-  err := errors.New("config:processor:getClosestTreeValue(): Path not found: " + strings.Join(requiredPath, "."))
+  log.Warn("Path not found: " + strings.Join(requiredPath, "."))
+  err := errors.New("Path not found: " + strings.Join(requiredPath, "."))
   return nil, err
 }
 
 func getAbsoluteTreeValue(requiredPath []string, config interface{}) (interface{}, error) {
-  // fmt.Printf("\nconfig:processor:getAbsoluteTreeValue(): %s\n%s\n", json.Encode(requiredPath), json.Encode(config))
-  fmt.Printf("\nconfig:processor:getAbsoluteTreeValue(): %s\n", json.Encode(requiredPath))
+  log.PushPath("getAbsoluteTreeValue")
+  defer log.PopPath()
+
   if len(requiredPath) == 0 {
-    fmt.Printf("len(requiredPath) == 0\n")
+    log.Info("len(requiredPath) == 0")
     return config, nil
   }
 
   for true {
-    fmt.Printf("requiredPath = [%s] %s\n", reflect.ValueOf(config).Kind().String(), strings.Join(requiredPath, "."))
+    log.Info("[" + reflect.ValueOf(config).Kind().String() + "] " + strings.Join(requiredPath, "."))
     switch reflect.ValueOf(config).Kind().String() {
     case "map":
       mapConfig := config.(map[string]interface{})
 
       // Break if next key doesn't exist
       if _, ok := mapConfig[requiredPath[0]]; !ok {
-        return nil, errors.New("config:processor:getAbsoluteTreeValue(): Path not found: " + json.Encode(requiredPath))
+        log.Warn("Path not found: " + strings.Join(requiredPath, "."))
+        return nil, errors.New("Path not found: " + strings.Join(requiredPath, "."))
         break
       }
 
       // If there's a single key left
       if len(requiredPath) == 1 {
-        fmt.Println(" > FOUND!")
+        log.Info("FOUND!")
         return mapConfig[requiredPath[0]], nil
       }
 
@@ -253,33 +253,49 @@ func getAbsoluteTreeValue(requiredPath []string, config interface{}) (interface{
       sliceConfig := config.([]interface{})
 
       // Make int key
-      fmt.Println(requiredPath[0])
       key, err := strconv.Atoi(requiredPath[0])
       if err != nil {
-        return nil, errors.New("config:processor:getAbsoluteTreeValue(): Couldn't make int out of string from '" + requiredPath[0] + "'")
+        log.Error("Couldn't make int out of string from '" + requiredPath[0] + "'")
+        return nil, errors.New("Couldn't make int out of string from '" + requiredPath[0] + "'")
       }
 
       // Break if next key doesn't exist
       if len(sliceConfig) < key {
-        fmt.Println(" > not found")
-        return nil, errors.New("config:processor:getAbsoluteTreeValue(): Path not found: " + json.Encode(requiredPath))
+        log.Warn("Path not found: " + strings.Join(requiredPath, "."))
+        return nil, errors.New("Path not found: " + strings.Join(requiredPath, "."))
       }
 
       // If there's a single key left
       if len(requiredPath) == 1 {
-        fmt.Println(" > FOUND!")
+        log.Info("FOUND!")
         return sliceConfig[key], nil
       }
 
       // Otherwise go deeper
       config = sliceConfig[key]
     default:
-      return nil, errors.New("config:processor:getAbsoluteTreeValue(): Got type [" + reflect.ValueOf(config).Kind().String() + "]")
+      log.Warn("Got type [" + reflect.ValueOf(config).Kind().String() + "]")
+      return nil, errors.New("Got type [" + reflect.ValueOf(config).Kind().String() + "]")
     }
 
     // Shift required path
     requiredPath = requiredPath[1:]
   }
 
-  return nil, errors.New("config:processor:getAbsoluteTreeValue(): Path not found: " + json.Encode(requiredPath))
+  log.Warn("Path not found: " + strings.Join(requiredPath, "."))
+  return nil, errors.New("Path not found: " + strings.Join(requiredPath, "."))
+}
+
+func preProcessRegexp() {
+  regex = make(map[string]*regexp.Regexp)
+  regex["variable"] = regexp.MustCompile("\\${([a-zA-Z0-9.-]+)}")
+}
+
+func contains(searchTerm string, list []string) bool {
+  for _, value := range list {
+    if value == searchTerm {
+      return true
+    }
+  }
+  return false
 }
