4e3340c6b5a2645731a99d359a48f06f731f2c8f
Gabi Ionescu <gabi.ionescu+git@protonmail.com>
2020-08-09 10:06:38 +0300

    Added db optimization launcher

---
+++ app/web/Core/Controller/OptimizeDB.php
@@ -0,0 +1,8 @@
+<?php
+
+class Core_Controller_OptimizeDB extends Core_Controller_Base {
+    public function run() {
+        $this->getState()->optimizeDB();
+        echo 'DONE';
+    }
+}
---
+++ app/web/optimize_db.php
@@ -0,0 +1,10 @@
+<?php
+
+// Load autoloader
+require "autoloader.php";
+
+// Initialize app
+$app = new Core_Controller_OptimizeDB;
+
+// Launch app
+$app->run();
