bda0766ee5a5f224c1019b9625eec89218e000f9
Gabriel Ionescu <gabi.ionescu+github@protonmail.com>
2020-09-02 01:09:21 +0300

    Formatting

--- app/modules/core/src/mqtt-stealer.go
+++ app/modules/core/src/mqtt-stealer.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+  "fmt"
+  "os"
+  "os/signal"
+  "syscall"
 
-	mqttDestination "./helpers/mqtt"
-	mqttSource "./helpers/mqtt"
+  mqttDestination "./helpers/mqtt"
+  mqttSource "./helpers/mqtt"
 )
 
 const serviceName = "core/mqtt-stealer"
@@ -16,20 +16,20 @@ const sourceBroker = "tcp://192.168.0.100:1883"
 const destinationBroker = "tcp://localhost:1883"
 
 func main() {
-	// Create channel to monitor interrupt signals
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+  // Create channel to monitor interrupt signals
+  c := make(chan os.Signal, 1)
+  signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	// Connect
-	mqttSource.Connect(serviceName, sourceBroker)
-	mqttDestination.Connect(serviceName, destinationBroker)
+  // Connect
+  mqttSource.Connect(serviceName, sourceBroker)
+  mqttDestination.Connect(serviceName, destinationBroker)
 
-	// Steal
-	mqttSource.SubscribeWithTopic("#", func(topic string, msg string) {
-		fmt.Println(topic + ": " + msg)
-		mqttDestination.PublishOn(topic, msg)
-	})
+  // Steal
+  mqttSource.SubscribeWithTopic("#", func(topic string, msg string) {
+    fmt.Println(topic + ": " + msg)
+    mqttDestination.PublishOn(topic, msg)
+  })
 
-	// Keep alive until interrupt is received
-	<-c
+  // Keep alive until interrupt is received
+  <-c
 }
--- app/run.sh
+++ app/run.sh
@@ -9,70 +9,70 @@ WEB_DIR="/app/web"
 
 # Data dir
 if [ ! -d "$DATA_DIR" ]; then
-    echo " > Creating data dir: $DATA_DIR"
-    mkdir -p "$DATA_DIR"
+  echo " > Creating data dir: $DATA_DIR"
+  mkdir -p "$DATA_DIR"
 fi
 
 # Config dir
 if [ ! -d "$CONFIG_DIR" ]; then
-    echo " > Creating config dir: $CONFIG_DIR"
-    mkdir -p "$CONFIG_DIR"
+  echo " > Creating config dir: $CONFIG_DIR"
+  mkdir -p "$CONFIG_DIR"
 fi
 
 # Config contents
 if [ "`ls -1 "$CONFIG_DIR" | wc -l`" == "0" ]; then
-    echo " > Populating config dir from $DEFAULT_CONFIG_DIR to $CONFIG_DIR"
-    rmdir "$CONFIG_DIR"
-    cp -R "$DEFAULT_CONFIG_DIR" "$CONFIG_DIR"
+  echo " > Populating config dir from $DEFAULT_CONFIG_DIR to $CONFIG_DIR"
+  rmdir "$CONFIG_DIR"
+  cp -R "$DEFAULT_CONFIG_DIR" "$CONFIG_DIR"
 
 # TODO: Remove in prod - sync modules config from default
 else
-    echo "[DEV] > Setting default modules config from $DEFAULT_CONFIG_DIR/Module to $CONFIG_DIR/Module"
-    rsync --delete-after --update -rvz "$DEFAULT_CONFIG_DIR/Module/" "$CONFIG_DIR/Module"
+  echo "[DEV] > Setting default modules config from $DEFAULT_CONFIG_DIR/Module to $CONFIG_DIR/Module"
+  rsync --delete-after --update -rvz "$DEFAULT_CONFIG_DIR/Module/" "$CONFIG_DIR/Module"
 fi
 
 # Permissions
 echo " > Make sure we've got permissions"
 chown $DOCKER_USERNAME:$DOCKER_GROUPID -R \
-    "$DATA_DIR"
+  "$DATA_DIR"
 chmod 775 -R \
-    "$DATA_DIR" \
-    "$WEB_DIR"
+  "$DATA_DIR" \
+  "$WEB_DIR"
 
 # Set hostname as the screen daemon name
 SCREEN_NAME="`hostname`"
 
 # Services to launch
 if [ "`cat /app/data/.env`" == "prod" ]; then
-    SERVICES='
-        mosquitto
-        mariadb
-        apache
-        mqtt-listener
-        full-state-provider
-        state-setter
-        function-run-listener
-        zigbee2mqtt
-        cec-client-mqtt-bridge
-        evdev2mqtt
-        broadlink2mqtt
-        cron
-        '
-        # bluetooth-scan
+  SERVICES='
+    mosquitto
+    mariadb
+    apache
+    mqtt-listener
+    full-state-provider
+    state-setter
+    function-run-listener
+    zigbee2mqtt
+    cec-client-mqtt-bridge
+    evdev2mqtt
+    broadlink2mqtt
+    cron
+    '
+    # bluetooth-scan
 else
-    SERVICES='
-        mosquitto
-        mariadb
-        core/mqtt-stealer
-        core/config
-        core/state
-        '
+  SERVICES='
+    mosquitto
+    mariadb
+    core/mqtt-stealer
+    core/config
+    core/state
+    '
 
-    echo " > Fetching GO dependencies..."
-    HERE="`pwd`"
-    cd /app/modules/core
-    go get -d ./...
-    cd "$HERE"
+  echo " > Fetching GO dependencies..."
+  HERE="`pwd`"
+  cd /app/modules/core
+  go get -d ./...
+  cd "$HERE"
 fi
 
 # Launch daemon
@@ -85,10 +85,10 @@ SCREEN_PID="`ps ax | grep screen | grep -v grep | awk '{print $1}'`"
 # Launch services
 i=0
 for service in $SERVICES ; do
-    i=$((i+1))
-    echo "   > Launch $service screen [$i]"
-    screen -S "$SCREEN_NAME" -X screen -t "$service"
-    screen -S "$SCREEN_NAME" -p $i -X stuff $'ash services/'${service}$'.service;exit\r'
+  i=$((i+1))
+  echo "   > Launch $service screen [$i]"
+  screen -S "$SCREEN_NAME" -X screen -t "$service"
+  screen -S "$SCREEN_NAME" -p $i -X stuff $'ash services/'${service}$'.service;exit\r'
 done
 
 # Close first unused screen
@@ -98,19 +98,19 @@ screen -S "$SCREEN_NAME" -p 0 -X stuff $'exit\r'
 # Safe stop when receiving the stop signal
 safed=0
 function safety() {
-	if [ $safed -eq 0 ]; then
-		safed=1
-		echo -e "\nAttempt safe stop...\n"
-		kill $SCREEN_PID &
-        while [ "`ps ax | grep screen | grep -v grep`" != "" ]; do
-            sleep 1s
-        done
-	fi
+  if [ $safed -eq 0 ]; then
+    safed=1
+    echo -e "\nAttempt safe stop...\n"
+    kill $SCREEN_PID &
+    while [ "`ps ax | grep screen | grep -v grep`" != "" ]; do
+      sleep 1s
+    done
+  fi
 }
 trap safety INT TERM EXIT
 
 # Keep self alive while the screen daemon is alive
 echo " > Will stay alive for the screen daemon"
 while [ "`ps ax | grep screen | grep -v grep`" != "" ]; do
-    sleep 300s
+  sleep 300s
 done
