791d000efde75c5b993319e389263573e0b2489c
Gabi Ionescu <gabi.ionescu+github@protonmail.com>
2021-03-24 22:02:00 +0200

    Highly improve data filtering

--- app/web/Core/Controller/Status.php
+++ app/web/Core/Controller/Status.php
@@ -6,23 +6,45 @@ class Core_Controller_Status extends Core_Controller_Base {
    */
   public function run() {
 
+    $object = isset($_GET['object']) ? explode(',', $_GET['object']) : [];
+    $param  = isset($_GET['param']) ? explode(',', $_GET['param']) : [];
+    $value  = isset($_GET['value']) ? explode(',', $_GET['value']) : [];
+
+    $no_object = isset($_GET['no-object']) ? explode(',', $_GET['no-object']) : [];
+    $no_param  = isset($_GET['no-param']) ? explode(',', $_GET['no-param']) : [];
+    $no_value  = isset($_GET['no-value']) ? explode(',', $_GET['no-value']) : [];
+
+    $no_from = isset($_GET['no-from']);
+    $no_time = isset($_GET['no-time']);
+
     // Get state
     $state = $this->getState()->getFullState();
 
     // Apply filters
-    if (isset($_GET['filter'])) {
-      $state = $this->_applyFilters($state, explode(',', $_GET['filter']));
-    }
+    $state = $this->_applyObjectFilters($state, $object);
+    $state = $this->_applyParamFilters($state, $param);
+    $state = $this->_applyValueFilters($state, $value);
+    $state = $this->_applyObjectExclusionFilters($state, $no_object);
+    $state = $this->_applyParamExclusionFilters($state, $no_param);
+    $state = $this->_applyValueExclusionFilters($state, $no_value);
+
+    $today = date('Y-m-d');
 
     // Fill temporary values waiting to be set
     foreach ($state AS $object => $variables) {
       foreach ($variables AS $variable => $tmp) {
-        // $tmp = $this->getState()->getVariable($object, $variable);
-        $state[$object][$variable] = $tmp['value']
-        . '<span style="color:lightgrey;">'
-        . ' from ' . $tmp['prevValue']
-        . ' @ ' . date('Y-m-d H:i:s', $tmp['timestamp'])
-          . '</span>';
+        $state[$object][$variable] = $tmp['value'] . '<span style="color:lightgrey;">';
+        if (!$no_from) {
+          $state[$object][$variable] .= ' from ' . $tmp['prevValue'];
+        }
+        if (!$no_time) {
+          if (date('Y-m-d', $tmp['timestamp']) == $today) {
+            $state[$object][$variable] .= ' @ ' . date('H:i:s', $tmp['timestamp']);
+          } else {
+            $state[$object][$variable] .= ' @ ' . date('Y-m-d H:i:s', $tmp['timestamp']);
+          }
+        }
+        $state[$object][$variable] .= '</span>';
 
         if ('' !== $tmp['tmpValue']) {
           $state[$object][$variable] .= ' (Detected "' . $tmp['tmpValue'] . '" ' . $tmp['tmpTimes'] . ' time' . (1 == $tmp['tmpTimes'] ? '' : 's') . ')';
@@ -37,22 +59,117 @@ class Core_Controller_Status extends Core_Controller_Base {
     echo $this->_drawHTML($state);
   }
 
-  /**
-   * @param  $state
-   * @param  $filters
-   * @return mixed
-   */
-  private function _applyFilters($state, $filters) {
-    if (!empty($filters)) {
-      $newState = [];
+  private function _applyObjectExclusionFilters($state, $filters) {
+    if (empty($filters)) {
+      return $state;
+    }
+    $newState = [];
+    foreach ($state AS $object => $values) {
+      $ok = true;
       foreach ($filters AS $filter) {
-        foreach ($state AS $object => $values) {
-          if (stripos($object, $filter) !== false) {
-            $newState[$object] = $values;
+        if (stripos($object, $filter) !== false) {
+          $ok = false;
+          break;
+        }
+      }
+      if ($ok) {
+        $newState[$object] = $values;
+      }
+    }
+    return $newState;
+  }
+
+  private function _applyObjectFilters($state, $filters) {
+    if (empty($filters)) {
+      return $state;
+    }
+    $newState = [];
+    foreach ($state AS $object => $values) {
+      foreach ($filters AS $filter) {
+        if (stripos($object, $filter) !== false) {
+          $newState[$object] = $values;
+        }
+      }
+    }
+    return $newState;
+  }
+
+  private function _applyParamExclusionFilters($state, $filters) {
+    if (empty($filters)) {
+      return $state;
+    }
+    $newState = [];
+    foreach ($state AS $object => $values) {
+      foreach ($values AS $key => $value) {
+        $ok = true;
+        foreach ($filters AS $filter) {
+          if (stripos($key, $filter) !== false) {
+            $ok = false;
+            break;
+          }
+        }
+        if ($ok) {
+          $newState[$object][$key] = $value;
+        }
+      }
+    }
+    return $newState;
+  }
+
+  private function _applyParamFilters($state, $filters) {
+    if (empty($filters)) {
+      return $state;
+    }
+    $newState = [];
+    foreach ($state AS $object => $values) {
+      foreach ($values AS $key => $value) {
+        foreach ($filters AS $filter) {
+          if (stripos($key, $filter) !== false) {
+            $newState[$object][$key] = $value;
+            break;
           }
         }
       }
-      $state = $newState;
+    }
+    return $newState;
+  }
+
+  private function _applyValueExclusionFilters($state, $filters) {
+    if (empty($filters)) {
+      return $state;
+    }
+    $newState = [];
+    foreach ($state AS $object => $values) {
+      foreach ($values AS $key => $value) {
+        $ok = true;
+        foreach ($filters AS $filter) {
+          if (stripos($value['value'], $filter) !== false) {
+            $ok = false;
+            break;
+          }
+        }
+        if ($ok) {
+          $newState[$object][$key] = $value;
+        }
+      }
+    }
+    return $newState;
+  }
+
+  private function _applyValueFilters($state, $filters) {
+    if (empty($filters)) {
+      return $state;
+    }
+    $newState = [];
+    foreach ($state AS $object => $values) {
+      foreach ($values AS $key => $value) {
+        foreach ($filters AS $filter) {
+          if (stripos($value['value'], $filter) !== false) {
+            $newState[$object][$key] = $value;
+            break;
+          }
+        }
+      }
     }
     return $newState;
   }
